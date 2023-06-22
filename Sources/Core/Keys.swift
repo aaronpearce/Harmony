4799d23104262890719be9b5d47cb6c40934bd42
Aaron Pearce <me@aaronpearce.com>
2023-06-23 08:52:39 +1200

    Adjust code file locations

--- Sources/Core/Keys.swift
+++ Sources/Core/Keys.swift
@@ -1,8 +1,14 @@
 //
-//  File.swift
-//  
+//  Keys.swift
+//
 //
 //  Created by Aaron Pearce on 22/06/23.
 //
 
 import Foundation
+
+extension Harmonic {
+    enum Keys {
+        static let stateSerialization = "Harmony.State"
+    }
+}
--- Sources/Extensions/StateSerialization+Encoder.swift
+++ Sources/Extensions/StateSerialization+Encoder.swift
@@ -1,8 +1,19 @@
 //
-//  File.swift
+//  StateSerialization+Encoder.swift
 //  
 //
 //  Created by Aaron Pearce on 22/06/23.
 //
 
-import Foundation
+import CloudKit
+
+extension CKSyncEngine.State.Serialization {
+
+    func encode() throws -> Data? {
+        return try JSONEncoder().encode(self)
+    }
+
+    static func decode(_ data: Data) throws -> Self? {
+        return try JSONDecoder().decode(Self.self, from: data)
+    }
+}
